5aea8a7ef688e189f59b4f2d3b0f0dadf67ec1f4
Suraj <suraj_k@tekditechnologies.com>
2024-01-17 14:04:57 +0530

    jobs backend

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Jobs-backend is running!!!';
+    return 'Jobs-dev-backend is running!';
   }
 }
