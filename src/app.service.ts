f771c50d0186e2251da60e064ad106baed202087
Suraj <suraj_k@tekditechnologies.com>
2024-01-20 18:07:43 +0530

    prod env

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Jobs-dev-backend is running!';
+    return 'Jobs-prod-backend is running!';
   }
 }
