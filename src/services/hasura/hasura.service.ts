fc34b079a5774f25cd7e8c3c092f674b4ece08a3
Suraj <suraj_k@tekditechnologies.com>
2024-02-23 07:59:52 +0530

    search query

--- src/services/hasura/hasura.service.ts
+++ src/services/hasura/hasura.service.ts
@@ -22,17 +22,22 @@ export class HasuraService {
     console.log("searching jobs from " + this.cache_db)
 
 
-    let result = 'where: {'
+    let result = 'where: {';
     Object.entries(getContentdto).forEach(([key, value]) => {
-      console.log(`${key}: ${value}`);
+      console.log(key, ':', value);
 
-      console.log("557", `${key}: ${value}`);
-      result += `${key}: {_eq: "${value}"}, `;
-
-
+      if (Array.isArray(value)) {
+        console.log('The value is an array');
+        const quotedValues = value.map(city => `"${city}"`).join(", ");
+        result += `${key}: {_in: [${quotedValues}]},`;
+      } else {
+        console.log('The value is not an array');
+        result += `${key}: {_in: "${value}"},`
+      }
+      
     });
-    result += '}'
-    console.log("result", result)
+    result += '}';
+    console.log("result", result);
     //console.log("order", order)
     const query = `query MyQuery {
            ${this.cache_db}(distinct_on: unique_id,${result}) {
@@ -110,7 +115,7 @@ export class HasuraService {
   async insertCacheData(arrayOfObjects) {
     console.log("inserting jobs into " + this.cache_db)
     console.log("arrayOfObjects", arrayOfObjects)
-    
+
     const query = `mutation MyMutation($item_id: String, $title: String, $description: String, $company: String, $location_id: String, $city: String, $state: String, $country: String, $qualification: String, $experience: String, $age_criteria: String, $skills: String, $proficiency: String, $work_mode: String, $start_date: String, $end_date: String, $responsiblities: String, $provider_id: String, $provider_name: String, $bpp_id: String, $bpp_uri: String, $unique_id: String, $gender: String ) { 
             insert_${this.cache_db}(objects: {item_id: $item_id, title: $title, description: $description, company: $company, location_id: $location_id, city: $city, state: $state, country: $country, qualification: $qualification, experience: $experience, age_criteria: $age_criteria, skills: $skills, proficiency: $proficiency, work_mode: $work_mode, start_date: $start_date, end_date: $end_date, responsiblities: $responsiblities, provider_id: $provider_id, provider_name: $provider_name, bpp_id: $bpp_id, bpp_uri: $bpp_uri, unique_id: $unique_id, gender: $gender }) {
             returning {
@@ -327,12 +332,12 @@ export class HasuraService {
     }
     `;
 
-   // console.log(query)
+    // console.log(query)
 
     // Rest of your code to execute the query
 
     try {
-      const response = await this.queryDb(query,order)
+      const response = await this.queryDb(query, order)
       return response;
     } catch (error) {
       throw new HttpException('Unabe to create order user', HttpStatus.BAD_REQUEST);
